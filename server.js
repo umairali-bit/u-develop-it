724404c9cb9a9c8b980303adc023d08b70d554c6
Umair Ali <umairmamoor@gmail.com>
2021-09-12 15:32:41 -0400

    delete sql function

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 
 
 // Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
 
 
 // Connect to database
@@ -30,37 +30,74 @@ const db = mysql.createConnection(
 
   // DB queries
   // SELECT * from candidates
-  db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+
+// Get all candidates
+app.get('/api/candidate', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
   });
 
   // GET a single candidate with the id = 1
-  db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-      if (err) {
-          console.log(err);
-      }
-      console.log(row);
+  app.get('/api/candidate/:id', (req, res) => {
+      const sql = `SELECT * FROM candidates WHERE id = ?`;
+      const params = [req.params.id];
+
+      db.query (sql, params, (err,row) => {
+          if (err) {
+              res.status(400).json({error: err.message});
+              return;
+          }
+          res.json ({
+              message: 'success',
+              data: row
+          });
+      });
   });
+ 
 
   // Delete a candidate
-  db.query(`DELETE FROM candidates WHERE id = ?`, 1 , (err, result) => {
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
       if (err) {
-          console.log(err);
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
       }
-      console.log(result);
-  })
-
-  // Create a candidate
-  const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-  VALUES (?,?,?,?)`;
-  const params = [1,'RONALD', 'FIRBANK', 1];
-
-  db.query(sql, params, (err, result) => {
-      if (err) {
-          console.log(err);
-      }
-      console.log(result);
+    });
   });
+
+//   // Create a candidate
+//   const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//   VALUES (?,?,?,?)`;
+//   const params = [1,'RONALD', 'FIRBANK', 1];
+
+//   db.query(sql, params, (err, result) => {
+//       if (err) {
+//           console.log(err);
+//       }
+//       console.log(result);
+//   });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
