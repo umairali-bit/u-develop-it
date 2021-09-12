af170d4233a15e6c2909080e7b2565749e302d7c
Umair Ali <umairmamoor@gmail.com>
2021-09-12 13:55:30 -0400

    server.js with all the dependencies and routes

---
+++ server.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql2');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '6102075b',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
