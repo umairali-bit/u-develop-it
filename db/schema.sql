f56fc541a7d284e8288e9ae2cf17f102a07bb199
Umair Ali <umairmamoor@gmail.com>
2021-09-13 14:47:40 -0400

    parties table

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE candidates (
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
 );
+
+
+INSERT INTO parties (name, description)
+VALUES
+  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
+  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
+  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
