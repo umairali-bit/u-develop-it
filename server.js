071795b352bb2e8b9dea8309a948b7169fdc86b9
Umair Ali <umairmamoor@gmail.com>
2021-09-12 20:54:37 -0400

    add curd routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const express = require('express');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -87,17 +88,28 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
   });
 
-//   // Create a candidate
-//   const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//   VALUES (?,?,?,?)`;
-//   const params = [1,'RONALD', 'FIRBANK', 1];
+  // Create a candidate
+  app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
 
-//   db.query(sql, params, (err, result) => {
-//       if (err) {
-//           console.log(err);
-//       }
-//       console.log(result);
-//   });
+    db.query(sql, params, (err, result) => {
+    if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+    } 
+      res.json({
+      message: 'success',
+      data: body
+      });
+    });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
