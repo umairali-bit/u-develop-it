9266ef013a9eb249c4f69413526d735fe7186f1d
Umair Ali <umairmamoor@gmail.com>
2021-09-14 12:36:42 -0400

    update a candidate's party function

--- server.js
+++ server.js
@@ -121,6 +121,37 @@ app.delete('/api/candidate/:id', (req, res) => {
   });
 
 
+  // Update a candidate's party
+  app.put('/api/candidate/:id', (req, res) => {
+  
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+
   // Parties api
 
   app.get('/api/parties', (req, res) => {
